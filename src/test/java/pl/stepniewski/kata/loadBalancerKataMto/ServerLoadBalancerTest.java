e1bf6cd75775ab4d14f047ea39944f048b3fb273
Arkadiusz Stępniewski <a.stepniewski@hotmail.com>
2015-04-23 21:35:06 +0200

    test 1 red

--- src/test/java/pl/stepniewski/kata/loadBalancerKataMto/ServerLoadBalancerTest.java
+++ src/test/java/pl/stepniewski/kata/loadBalancerKataMto/ServerLoadBalancerTest.java
@@ -10,4 +10,13 @@ public class ServerLoadBalancerTest {
 	public void itCompiles() {
 		assertThat(true, equalTo(true));
 	}
+	
+	@Test
+		public void balancingAServer_noVms_serverStaysEmpty(){
+			Server theServer = a(server().withCapacity(1));
+			
+			balance(aListOfServersWith(theServer), anEmptyListOfVms());
+			
+			assertThat(theServer, hasLoadPercentageOf(0.0d));
+		}
 }
