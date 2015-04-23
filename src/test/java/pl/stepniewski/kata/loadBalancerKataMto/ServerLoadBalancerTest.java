6900fd9ece98311cfe603c546f340eb35ebaf985
Arkadiusz Stępniewski <a.stepniewski@hotmail.com>
2015-04-23 21:22:05 +0200

    initial test commit

---
+++ src/test/java/pl/stepniewski/kata/loadBalancerKataMto/ServerLoadBalancerTest.java
@@ -0,0 +1,13 @@
+package pl.stepniewski.kata.loadBalancerKataMto;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Test;
+
+public class ServerLoadBalancerTest {
+	@Test
+	public void itCompiles() {
+		assertThat(true, equalTo(true));
+	}
+}
